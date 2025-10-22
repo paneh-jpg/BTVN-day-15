18a11b8c6163ec97e41906aa2302d67d3c9d1568
paneh-jpg <vtpanh39.work@gmail.com>
2025-10-22 21:38:43 +0700

    Test03

--- js/ex04.js
+++ js/ex04.js
@@ -26,32 +26,8 @@ jerry.getName = function () {
 console.log(jerry.getName());
 */
 
-// 1) Object thường
-const o = { a: 1 };
-console.log(o + ""); // "[object Object]"
-console.log(String(o)); // "[object Object]"
-
-// 2) Array
-console.log([1, 2, 3] + ""); // "1,2,3"
-console.log(String([1, 2, 3])); // "1,2,3"
-
-// 3) Date
-const d = new Date();
-console.log(d + ""); // "Wed Oct ..." (toString())
-console.log(+d); // 1729... (ms)  -> valueOf()
-
-// 4) Number wrapper
-const n = new Number(999);
-console.log(n + ""); // "999" (valueOf() -> 999)
-
-// 5) Ưu tiên theo hint
-const obj = {
-  valueOf() {
-    return 10;
-  },
-  toString() {
-    return "ten";
-  },
+const person = {
+  name: "John",
 };
-console.log(obj + 1); // 11   (number-hint: valueOf() trước)
-console.log(`${obj}`); // "ten" (string-hint: toString() trước)
+
+console.log(person.toString());
